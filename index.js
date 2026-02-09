7b858ae6b310da91953555df838ed26442b5ba4e
gapn <82774730+gapn@users.noreply.github.com>
2026-02-10 00:57:42 +0100

    changed code

--- index.js
+++ index.js
@@ -1,85 +1,58 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+const urlparser = require('url');
 const dns = require('dns');
-const urlParser = require('url');
 
-mongoose.connect(process.env.MONGODB_URI)
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db('urlshortener');
+const urls = db.collection('urls');
 
-const urlSchema = new mongoose.Schema({
-  original_url: String,
-  short_url: Number
-});
+const port = process.env.PORT || 3000;
 
-const Url = mongoose.model('Url', urlSchema);
-
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose is connected to the cluster!');
-});
-
-//middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-//routes
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get('/', (req, res) => res.sendFile(process.cwd() + '/views/index.html'));
 
-//POST route
-app.post('/api/shorturl', async(req, res) => {
+app.post('/api/shorturl', (req, res) => {
   const originalUrl = req.body.url;
+  const hostname = urlparser.parse(originalUrl).hostname;
 
-  try {
-    const urlObject = new URL(originalUrl);
+  if (!hostname) {
+    return res.json({ error: 'invalid url' });
+  }
 
-    if (urlObject.protocol !== 'http:' && urlObject.protocol !== 'https:') {
+  dns.lookup(hostname, async (err, address) => {
+    if (!address) {
       return res.json({ error: 'invalid url' });
     }
 
-    dns.lookup(urlObject.hostname, async (err) => {
-      if (err) {
-        res.json({ error: 'invalid url' });
-      } else {
-        // Check if it exists in DB already
-        let foundUrl = await Url.findOne({ original_url: originalUrl });
+    const urlCount = await urls.countDocuments({});
+    const urlDoc = {
+      url: originalUrl,
+      short_url: urlCount + 1 // Start at 1 instead of 0
+    };
 
-        if (foundUrl) {
-          res.json({ original_url: foundUrl.original_url, short_url: foundUrl.short_url });
-        } else {
-          // Get count to create the next short_url ID
-          const count = await Url.countDocuments();
-          const newUrl = new Url({
-            original_url: originalUrl,
-            short_url: count + 1
-          });
-          await newUrl.save();
-          res.json({ original_url: newUrl.original_url, short_url: newUrl.short_url });
-        }
-      }
-    })
-  } catch (err) {
-    res.json({ error: 'invalid url' });
-  }
+    await urls.insertOne(urlDoc);
+    res.json({ original_url: originalUrl, short_url: urlCount + 1 });
+  });
 });
 
-//GET route
 app.get('/api/shorturl/:short_url', async (req, res) => {
   const shorturl = req.params.short_url;
-  const foundUrl = await Url.findOne({ short_url: Number(shorturl) });
-
-  if (foundUrl) {
-    return res.redirect(foundUrl.original_url);
+  const urlDoc = await urls.findOne({ short_url: +shorturl });
+  
+  if (urlDoc) {
+    // Standard 302 redirect
+    return res.redirect(urlDoc.url);
   } else {
-    return res.json({ error: "No short URL found for your input" });
+    return res.json({ error: "No short URL found" });
   }
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}`));
